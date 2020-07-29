b4bd372760ebeaf9ea224982ddb240085dc8767f
Larry W Jordan Jr <larouex@gmail.com>
2020-07-28 17:56:21 -0700

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # IoTC-OPCUA-Server-Basic
-Base testing application for OPC/UA Scenarios for Azure IoT Central
+OPC/UA Server for Base Testing Scenarios for Azure IoT Central
+
+# Raspberry Pi "Translation" Gateway for Azure IoT Central
+
+![alt text](./Assets/raspberry-pi-4-hero-shot.png "Project Hero Shot") 
+
+## Overview
+This repository is part of a training and project series for Azure IoT Central.
+
+## Features
+This is a simple OPC/UA Server written in Python using the <b>opcua-asyncio</b> that is based on the popular <b>FreeOpcUa</b> project/library.
+
+[LINK: opcua-asyncio](https://github.com/FreeOpcUa/opcua-asyncio)
+
+The server expresses two Nodes...
+
+  * Ambient
+  * Process
+
+The <b>Ambient</b> node emits the following parameters...
+
+  * Temperature
+  * Humidity
+  
+The <b>Process</b> node emits the following parameters...
+
+  * Temperature
+  * Pressure
+  * Mixing Ratio
+
+## Setting up Your Development Toolchain
+The code in this repository depends on Visual Studio Code and Python.
+
+### Your Local Machine
+The development "toolchain" refers to all of the various tools, SDK's and bits we need to install on your machine to facilitate a smooth experience developing our project. Our main development tool will be Visual Studio code. 
+
+| - | Install These Tools |
+|---|---|
+| ![Python](./Assets/python-icon-100.png) | [LINK: Python 3 Installation Page](https://www.python.org/downloads/) - Pyhon is the programming language we will use to build applications for the Raspberry Pi. |
+| ![Visual Studio Code](./Assets/vs-code-icon-100.png) | [LINK: Visual Studio Code Installation Page](https://code.visualstudio.com/download) - Visual Studio Code is a lightweight but powerful source code editor which runs on your desktop and is available for Windows, macOS and Linux. This is the IDE we will use to write code and deploy to the our BLE Devices and the Raspberry Pi Gateway.  |
+| ![Docker](./Assets/docker-icon-100.png) | [LINK: Docker Desktop Install](https://www.docker.com/products/docker-desktop) - Docker Desktop is an application for MacOS and Windows machines for the building and sharing of containerized applications. |
+
+### Upgrading pip
+Pip is the package manager we will use to download packages
+
+On Linux or macOS (Open Terminal):
+```
+    pip install -U pip
+```
+On Windows (from a CMD window or Powershell):
+```
+    python -m pip install -U pip
+```
+
+### Install all the Tools for Visual Studio Code
+These are a set of tools we will use to develop our apps on the Raspberry Pi. You can open the Extensions sidebar with "Shift+Ctrl+X) or click the icon in the side navigator bar.
+
+![alt text](./Assets/vs-code-python-sml.png "VS Code Python")
+
+![alt text](./Assets/vs-code-docker-sml.png "VS Code Docker")
+
---
+++ server.py
@@ -0,0 +1,89 @@
+# ==================================================================================
+#   File:   gateway.py
+#   Author: Larry W Jordan Jr (larouex@gmail.com)
+#   Use:    Raspberry Pi Gateway for Azure IoT Central implementing OPC/UA
+#           This is the main application to read from 
+#
+#   Online:   www.hackinmakin.com
+#
+#   (c) 2020 Larouex Software Design LLC
+#   This code is licensed under MIT license (see LICENSE.txt for details)    
+# ==================================================================================
+import  getopt, sys, time, string, threading, asyncio, os
+import logging as Log
+
+# our classes
+from classes.provisiondevices import ProvisionDevices
+from classes.config import Config
+
+# -------------------------------------------------------------------------------
+#   Provision Devices
+# -------------------------------------------------------------------------------
+async def provision_devices(ProvisioningScope, GatewayType):
+
+  provisiondevices = ProvisionDevices(Log, ProvisioningScope, GatewayType)
+  await provisiondevices.provision_devices()
+  return True
+
+async def main(argv):
+
+    # execution state from args
+    provisioning_scope = None
+    gateway_type = None
+
+    short_options = "hvp:g:"
+    long_options = ["help", "verbose", "provisioningscope=", "gatewaytype="]
+    full_cmd_arguments = sys.argv
+    argument_list = full_cmd_arguments[1:]
+    try:
+        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    except getopt.error as err:
+        print (str(err))
+    
+    for current_argument, current_value in arguments:
+        if current_argument in ("-h", "--help"):
+            print("HELP for provisiondevices.py")
+            print("------------------------------------------------------------------------------------------------------------------")
+            print("-h or --help - Print out this Help Information")
+            print("-v or --verbose - Debug Mode with lots of Data will be Output to Assist with Debugging")
+            print("-p or --provisioningscope - Provisioning Scope give you fine grained control over the devices you want to provision.")
+            print("    ALL - Re-Provision Every device listed in the DevicesCache.json file")
+            print("    NEW - Only Provision Devices DevicesCache.json file that have 'LastProvisioned=Null'")
+            print("    device name - Provision a Specifc Device in DevicesCache.json file")
+            print("-g or --gatewaytype - Indicate the Type of Gateway Relationship")
+            print("    OPAQUE - Devices will look like Stand-Alone Devices in IoT Central")
+            print("    TRANSPARENT - Devices will look like Stand-Alone Devices in IoT Central")
+            print("    PROTOCOL - IoT Central will show a Single Gateway and all Data is Associated with the Gateway")
+            print("    PROTOCOLWITHIDENTITY - IoT Central will show a Single Gateway and Leaf Devices")
+            print("------------------------------------------------------------------------------------------------------------------")
+            sys.exit()
+        
+        if current_argument in ("-v", "--verbose"):
+            Log.basicConfig(format="%(levelname)s: %(message)s", level=Log.DEBUG)
+            Log.info("Verbose mode...")
+        else:
+            Log.basicConfig(format="%(levelname)s: %(message)s")
+        
+        if current_argument in ("-p", "--provisioningscope"):
+            Log.info("Provisioning Scope Override...%s" % current_value)
+            provisioning_scope = current_value
+
+        if current_argument in ("-g", "--gatewaytype"):
+            Log.info("Gateway Type  Override...%s" % current_value)
+            gateway_type = current_value
+
+    # Load Configuration File
+    config = Config(Log)
+    config_data = config.data
+    
+    if provisioning_scope == None:
+      provisioning_scope = config_data["ProvisioningScope"]
+
+    if gateway_type == None:
+      gateway_type = config_data["GatewayType"]
+
+    await provision_devices(ProvisioningScope=provisioning_scope, GatewayType=gateway_type)
+
+if __name__ == "__main__":
+    asyncio.run(main(sys.argv[1:]))
+
